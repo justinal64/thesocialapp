fb7a9c82c9ca1afa21b0c2cea12cdc844d20a566
justin leggett <justin@codingrookie.com>
2017-06-05 14:15:12 -0500

    added react-base

--- src/routes/community/community.js
+++ src/routes/community/community.js
@@ -2,13 +2,13 @@ import React, { Component } from 'react';
 import {
   AppRegistry,
   StyleSheet,
-  Text,
   View,
-  Button,
   ScrollView,
-  TextInput
+  TextInput,
+  Image
 } from 'react-native';
-
+import { Container, Header, Title, Content, Button, Icon, List, ListItem, Text, Thumbnail } from 'native-base';
+import styles from './styles';
 import axios from 'axios';
 
 
@@ -35,38 +35,57 @@ export default class Community extends React.Component {
     const { navigate } = this.props.navigation;
     return (
       <ScrollView>
-        { this.state.userData.map((user, key) => (
-          <View key={user.id}>
-            <Text>Name: {user.name}</Text>
-            <Text>Email: {user.email}</Text>
-            <Text>Username: {user.username}</Text>
-            <Text>Phone#: {user.phone}</Text> 
-            <Text>City: {user.address.city}</Text>                                         
-          </View>
-        ))}
+          {/*{ this.state.userData.map((user, key) => (
+            <View key={user.id}>
+              <Image source={require('../../imgs/user.jpg')} style={{width: 50, height: 50}}/>
+              <Text>Name: {user.name}</Text>
+              <Text>Email: {user.email}</Text>
+              <Text>Username: {user.username}</Text>
+              <Text>Phone#: {user.phone}</Text> 
+              <Text>City: {user.address.city}</Text>                                         
+            </View>
+          ))}*/}
+          
+          
+        <Container style={styles.container}>
+          <Header>
+            <Title>Community Activity</Title>
+          </Header>
+
+          <Content>
+            <List>
+              <ListItem>
+                <Thumbnail square size={80} source={require('../../imgs/user.jpg')} />
+                <Text>Justin Smith</Text>
+                <Text note>Check out this great article. </Text>
+              </ListItem>
+              <ListItem>
+                <Thumbnail square size={80} source={require('../../imgs/user.jpg')} />
+                <Text>Katie Smith</Text>
+                <Text note>One needs courage to be happy and smiling all time . . </Text>
+              </ListItem>
+              <ListItem>
+                <Thumbnail square size={80} source={require('../../imgs/user.jpg')} />
+                <Text>Kallie Smith</Text>
+                <Text note>Live a life style that matchs your vision</Text>
+              </ListItem>
+              <ListItem>
+                <Thumbnail square size={80} source={require('../../imgs/user.jpg')} />
+                <Text>Zoey Smith</Text>
+                <Text note>Failure is temporary, giving up makes it permanent</Text>
+              </ListItem>
+              <ListItem>
+                <Thumbnail square size={80} source={require('../../imgs/user.jpg')} />
+                <Text>Pax Smith</Text>
+                <Text note>The biggest risk is a missed opportunity !!</Text>
+              </ListItem>
+            </List>
+          </Content>
+        </Container>
       </ScrollView>
     );
   }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
-});
 
 AppRegistry.registerComponent('KFL', () => Community);
 
