27fa5b117aa156bfd1ac05c91b5b7acec9be906f
justin leggett <justin@codingrookie.com>
2017-06-06 11:08:34 -0500

    added nativebase styling to community page

--- src/routes/community/community.js
+++ src/routes/community/community.js
@@ -1,16 +1,10 @@
 import React, { Component, Linking } from 'react';
 import {
   AppRegistry,
-  StyleSheet,
-  View,
-  ScrollView,
-  TextInput,
   Image,
-  Container,
-  Text
 } from 'react-native';
+import { Container, Content, Card, CardItem, Thumbnail, Text, Button, Icon, Left, Body, Header } from 'native-base';
 import axios from 'axios';
-
 
 
 export default class Community extends React.Component {
@@ -19,8 +13,7 @@ export default class Community extends React.Component {
   };
     constructor(props) {
     super(props);
-    this.state = { jsonData: [],
-                    userData: null}; 
+    this.state = { userData: null}; 
   }
   
   componentDidMount() {
@@ -35,23 +28,37 @@ export default class Community extends React.Component {
     if(this.state.userData === null) return null;
     const { navigate } = this.props.navigation;
     return (
-      <ScrollView>
-          { this.state.userData.map((user, key) => (
-            <View key={user.id}>
-              {/*<Image source={require('../../imgs/user.jpg')} style={{width: 50, height: 50}}/>
-              <Text>Name: {user.name}</Text>
-              <Text>Email: {user.email}</Text>
-              <Text>Username: {user.username}</Text>
-              <Text>Phone#: {user.phone}</Text> 
-              <Text>City: {user.address.city}</Text>   
-                                                    */}
-                <Image source={require('../../imgs/user.jpg')} style={{width: 50, height: 50}}/>
-                <Text>{user.username} Check out my website it is a really cool website</Text>
-                <Text style={{color: 'blue'}} onPress={() => Linking.openURL('https://google.com')}> {user.website} </Text>
-              </View>
-          ))}
-          
-      </ScrollView>
+              <Container>
+                <Header>
+                  <Text>Community</Text>
+                </Header>
+                <Content>
+                    { this.state.userData.map((user, key) => (
+                  <Card key={user.id}>
+                    <CardItem>
+                      <Left>
+                        <Thumbnail source={require('../../imgs/user.jpg')} />
+                        <Body>
+                            <Text>{user.username}</Text>
+                            <Text note>{user.company.name}</Text>
+                            <Text>11h ago</Text>
+                        </Body>
+                      </Left>
+                    </CardItem>
+                    <CardItem cardBody>
+                      <Image/>
+                    </CardItem>
+                    <CardItem>
+                          {/*<Text>11h ago</Text>*/}
+                      {/*<Button transparent>*/}
+                          <Text>This is a super long comment This is a super long comment This is a super long comment</Text>
+                      {/*</Button>*/}
+                    </CardItem>
+                   </Card>                                   
+            ))}
+                        
+                </Content>
+            </Container>
     );
   }
 }
@@ -60,3 +67,17 @@ export default class Community extends React.Component {
 
 AppRegistry.registerComponent('KFL', () => Community);
 
+          // { this.state.userData.map((user, key) => (
+          //   <View key={user.id}>
+              {/*<Image source={require('../../imgs/user.jpg')} style={{width: 50, height: 50}}/>
+              <Text>Name: {user.name}</Text>
+              <Text>Email: {user.email}</Text>
+              <Text>Username: {user.username}</Text>
+              <Text>Phone#: {user.phone}</Text> 
+              <Text>City: {user.address.city}</Text>   
+                                                    */}
+          //       <Image source={require('../../imgs/user.jpg')} style={{width: 50, height: 50}}/>
+          //       <Text>{user.username} Check out my website it is a really cool website</Text>
+          //       <Text style={{color: 'blue'}} onPress={() => Linking.openURL('https://google.com')}> {user.website} </Text>
+          //     </View>
+          // ))}
